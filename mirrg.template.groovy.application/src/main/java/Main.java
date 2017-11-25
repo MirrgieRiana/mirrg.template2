e21c2fc75184ed5bcd2990eb37df504d1f061022
Mirrgie Riana <tacticsrealize2@gmail.com>
2017-11-26 08:56:00 +0900

    groovy.application: add: Main

---
+++ mirrg.template.groovy.application/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame("Template");
+
+		JLabel label = new JLabel("Sample");
+		label.setPreferredSize(new Dimension(200, 100));
+		frame.add(label);
+
+		frame.pack();
+		frame.setLocationByPlatform(true);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
